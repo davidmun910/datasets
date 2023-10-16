5a3af398334eb3f6afb1d06cb7b6ec706298575d
David Mun <davidm16@illinois.edu>
2023-10-16 17:26:43 -0500

    Finished creating SPRING 2024 CSV

--- course-catalog/script/all.sh
+++ course-catalog/script/all.sh
@@ -1,7 +1,2 @@
-node CIS-scraper.js 2019-fa
-node CIS-scraper.js 2019-sp
-node CIS-scraper.js 2018-fa
-node CIS-scraper.js 2018-sp
-node CIS-scraper.js 2017-fa
-node CIS-scraper.js 2017-sp
-node CIS-scraper.js 2016-fa
+node CIS-scraper.js 2023-fa
+node CIS-scraper.js 2024-sp
